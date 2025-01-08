c61b65c37cdf1047302dd1ec0b8dc13a1e60e6e6
andres-geotec <andres.geotec@gmail.com>
2025-01-08 00:07:03 -0600

    🔧 feat: vista post con markdown

---
+++ pages/[slug].vue
@@ -0,0 +1,25 @@
+<script setup>
+import { marked } from "marked";
+
+const x = marked('# Title\n\n## Second title\n\nLorem ipsum dolor sit amet')
+</script>
+
+<template>
+  <div class="contenedor">
+    <div class="ancho-fijo">
+      <h1>Mi primer post</h1>
+      <p class="parrafo-texto-alto m-y-0">Por: </p>
+      <p class="nota m-y-0">Fecha de publicación: </p>
+      <p>
+        <b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi magni cupiditate unde suscipit harum facere. Rerum suscipit accusantium voluptas earum cumque, inventore quos soluta tempora officiis ex, assumenda eius totam.</b>
+      </p>
+      <figure>
+        <img src="" alt="">
+        <figcaption>Portada - </figcaption>
+      </figure>
+      <!-- {{ useRoute().params }} -->
+    </div>
+
+    <div class="ancho-lectura" v-html="x" />
+  </div>
+</template>
